6930941f66a46a706eea4d2d0f0a3cdcd49c2674
nikithasri-168 <nikithasrigarapati@gmail.com>
2024-03-27 12:09:39 -0500

    update cars

---
+++ routes/cars.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+const cars = [
+  { make: 'Toyota', model: 'Camry', year: 2022 },
+  { make: 'Honda', model: 'Civic', year: 2021 },
+  { make: 'Suzuki', model: 'Alto', year: 2021 },
+];
+
+router.get('/', (req, res) => {
+  res.render('cars', { title: 'Search Results - Cars', cars });
+});
+
+module.exports = router;
